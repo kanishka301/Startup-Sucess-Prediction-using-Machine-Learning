33021eab9eead63958e36ade7c15412806896016
kanishka301 <203.kanishka@gmail.com>
2025-06-29 00:38:36 +0530

    Added files via upload

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -4,19 +4,16 @@ from dataclasses import dataclass
 import pandas as pd
 import numpy as np
 
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from sklearn.compose import ColumnTransformer
-
 from src.exception import CustomException
 from src.logger import logging
 from src.utils import save_object
 
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.compose import ColumnTransformer
 
 @dataclass
 class DataTransformationConfig:
     preprocessor_obj_file_path: str = os.path.join("artifacts", "preprocessor.pkl")
-
 
 class DataTransformation:
     def __init__(self):
@@ -24,19 +21,11 @@ class DataTransformation:
 
     def get_data_transformer_object(self, input_features: list):
         try:
-            logging.info("Creating standard scaling pipeline for numeric features.")
-
-            num_pipeline = Pipeline(steps=[
-                ("scaler", StandardScaler())
+            passthrough_transformer = ColumnTransformer([
+                ("pass_through", FunctionTransformer(validate=False), input_features)
             ])
-
-            preprocessor = ColumnTransformer(
-                transformers=[
-                    ("num_pipeline", num_pipeline, input_features)
-                ]
-            )
-
-            return preprocessor
+            logging.info("Returning passthrough transformer (no scaling).")
+            return passthrough_transformer
 
         except Exception as e:
             raise CustomException(e, sys)
@@ -50,19 +39,16 @@ class DataTransformation:
             target_column = "status"
             input_features = [col for col in train_df.columns if col != target_column]
 
-            logging.info("Obtaining preprocessor object.")
             preprocessor = self.get_data_transformer_object(input_features)
 
-            X_train = train_df.drop(columns=[target_column])
+            X_train = train_df[input_features]
             y_train = train_df[target_column]
 
-            X_test = test_df.drop(columns=[target_column])
+            X_test = test_df[input_features]
             y_test = test_df[target_column]
 
-            logging.info("Fitting and transforming training data.")
-            X_train_scaled = preprocessor.fit_transform(X_train)
-            logging.info("Transforming test data.")
-            X_test_scaled = preprocessor.transform(X_test)
+            X_train_processed = preprocessor.fit_transform(X_train)
+            X_test_processed = preprocessor.transform(X_test)
 
             logging.info("Saving preprocessor object.")
             save_object(
@@ -70,8 +56,8 @@ class DataTransformation:
                 obj=preprocessor
             )
 
-            train_arr = np.c_[X_train_scaled, np.array(y_train)]
-            test_arr = np.c_[X_test_scaled, np.array(y_test)]
+            train_arr = np.c_[X_train_processed, np.array(y_train)]
+            test_arr = np.c_[X_test_processed, np.array(y_test)]
 
             return (
                 train_arr,
--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+
 from src.exception import CustomException
 from src.utils import load_object
 from src.components.data_transformation import DataTransformationConfig
@@ -16,74 +17,112 @@ class PredictPipeline:
             model_path = ModelTrainerConfig().trained_model_file_path
             preprocessor_path = DataTransformationConfig().preprocessor_obj_file_path
 
+            # Load model and preprocessor objects
             model = load_object(model_path)
             preprocessor = load_object(preprocessor_path)
 
-            # Transform input data
+            # Apply transformation (no scaling, just passthrough)
             transformed_data = preprocessor.transform(features)
 
-            # Make prediction
+            # Predict
             prediction = model.predict(transformed_data)
-
             return prediction
+
         except Exception as e:
             raise CustomException(e, sys)
 
 
 class CustomData:
-    def __init__(self,
-                 age_first_funding_year: float,
-                 age_last_funding_year: float,
-                 age_first_milestone_year: float,
-                 age_last_milestone_year: float,
-                 funding_rounds: int,
-                 funding_total_usd: float,
-                 milestones: int,
-                 is_CA: int,
-                 is_NY: int,
-                 is_MA: int,
-                 is_TX: int,
-                 is_otherstate: int,
-                 is_software: int,
-                 is_web: int,
-                 is_mobile: int,
-                 is_enterprise: int,
-                 is_advertising: int,
-                 is_gamesvideo: int,
-                 is_ecommerce: int,
-                 is_biotech: int,
-                 is_consulting: int,
-                 is_othercategory: int,
-                 has_VC: int,
-                 has_angel: int,
-                 has_roundA: int,
-                 has_roundB: int,
-                 has_roundC: int,
-                 has_roundD: int,
-                 avg_participants: float,
-                 is_top500: int,
-                 founded_at_month: int,
-                 founded_at_day: int,
-                 founded_at_year: int,
-                 closed_at_month: int,
-                 closed_at_day: int,
-                 closed_at_year: int,
-                 has_RoundABCD: int,
-                 has_Investor: int,
-                 has_Seed: int,
-                 invalid_startup: int,
-                 tier_relationships: int
-                 ):
-        self.age_first_funding_year = age_first_funding_year
-        self.age_last_funding_year = age_last_funding_year
-        self.age_first_milestone_year = age_first_milestone_year
-        self.age_last_milestone_year = age_last_milestone_year
-        self.funding_rounds = funding_rounds
-        self.funding_total_usd = funding_total_usd
-        self.milestones = milestones
-        self.is_CA = is_CA
-        self.is_NY = is_NY
-        self.is_MA = is_MA
-        self.is_TX = is_TX
-        self.is_otherstate = is_otherstate
-        self.is_software = is_soft
+    def __init__(
+        self,
+        age_first_funding_year: float,
+        age_last_funding_year: float,
+        age_first_milestone_year: float,
+        age_last_milestone_year: float,
+        funding_rounds: float,
+        funding_total_usd: float,
+        milestones: float,
+        is_CA: int,
+        is_NY: int,
+        is_MA: int,
+        is_TX: int,
+        is_otherstate: int,
+        is_software: int,
+        is_web: int,
+        is_mobile: int,
+        is_enterprise: int,
+        is_advertising: int,
+        is_gamesvideo: int,
+        is_ecommerce: int,
+        is_biotech: int,
+        is_consulting: int,
+        is_othercategory: int,
+        has_VC: int,
+        has_angel: int,
+        has_roundA: int,
+        has_roundB: int,
+        has_roundC: int,
+        has_roundD: int,
+        avg_participants: float,
+        is_top500: int,
+        founded_at_month: int,
+        founded_at_day: int,
+        founded_at_year: int,
+        closed_at_month: int,
+        closed_at_day: int,
+        closed_at_year: int,
+        has_RoundABCD: int,
+        has_Investor: int,
+        has_Seed: int,
+        invalid_startup: int,
+        tier_relationships: float,
+    ):
+        self.data = {
+            "age_first_funding_year": [age_first_funding_year],
+            "age_last_funding_year": [age_last_funding_year],
+            "age_first_milestone_year": [age_first_milestone_year],
+            "age_last_milestone_year": [age_last_milestone_year],
+            "funding_rounds": [funding_rounds],
+            "funding_total_usd": [funding_total_usd],
+            "milestones": [milestones],
+            "is_CA": [is_CA],
+            "is_NY": [is_NY],
+            "is_MA": [is_MA],
+            "is_TX": [is_TX],
+            "is_otherstate": [is_otherstate],
+            "is_software": [is_software],
+            "is_web": [is_web],
+            "is_mobile": [is_mobile],
+            "is_enterprise": [is_enterprise],
+            "is_advertising": [is_advertising],
+            "is_gamesvideo": [is_gamesvideo],
+            "is_ecommerce": [is_ecommerce],
+            "is_biotech": [is_biotech],
+            "is_consulting": [is_consulting],
+            "is_othercategory": [is_othercategory],
+            "has_VC": [has_VC],
+            "has_angel": [has_angel],
+            "has_roundA": [has_roundA],
+            "has_roundB": [has_roundB],
+            "has_roundC": [has_roundC],
+            "has_roundD": [has_roundD],
+            "avg_participants": [avg_participants],
+            "is_top500": [is_top500],
+            "founded_at_month": [founded_at_month],
+            "founded_at_day": [founded_at_day],
+            "founded_at_year": [founded_at_year],
+            "closed_at_month": [closed_at_month],
+            "closed_at_day": [closed_at_day],
+            "closed_at_year": [closed_at_year],
+            "has_RoundABCD": [has_RoundABCD],
+            "has_Investor": [has_Investor],
+            "has_Seed": [has_Seed],
+            "invalid_startup": [invalid_startup],
+            "tier_relationships": [tier_relationships]
+        }
+
+    def get_data_as_df(self):
+        try:
+            return pd.DataFrame(self.data)
+        except Exception as e:
+            raise CustomException(e, sys)
